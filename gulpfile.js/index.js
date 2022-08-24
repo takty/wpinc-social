159fd269a72d4fac5dd35439377c07100eb7e9da
Takuto Yanagida <yanagida@stxst.com>
2022-08-24 11:59:30 +0900

    Change gulp process

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -2,29 +2,33 @@
  * Gulp file
  *
  * @author Takuto Yanagida
- * @version 2022-03-23
+ * @version 2022-08-23
  */
 
 /* eslint-disable no-undef */
 'use strict';
 
-const SRC_PHP = ['src/**/*.php'];
-const DEST    = './dist';
+const SRC_JS_RAW = ['src/**/*.js', '!src/**/*.min.js'];
+const SRC_PHP    = ['src/**/*.php'];
+const DEST       = './dist';
 
 const gulp = require('gulp');
 
 const { makeCopyTask } = require('./task-copy');
+const { makeJsTask }   = require('./task-js');
 
 
 // -----------------------------------------------------------------------------
 
 
+const js  = makeJsTask(SRC_JS_RAW, DEST, 'src');
 const php = makeCopyTask(SRC_PHP, DEST);
 
 const watch = done => {
+	gulp.watch(SRC_JS_RAW, gulp.series(js));
 	gulp.watch(SRC_PHP, gulp.series(php));
 	done();
 };
 
-exports.build   = gulp.parallel(php);
+exports.build   = gulp.parallel(js, php);
 exports.default = gulp.series(exports.build , watch);
